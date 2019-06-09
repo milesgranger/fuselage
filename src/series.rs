0f2ce0ad8ec5d6fa604f7056e9d1466a9d8701b2
Miles Granger <miles59923@gmail.com>
2019-06-09 16:40:07 +0200

    Add more macros for mapping

--- src/series.rs
+++ src/series.rs
@@ -1,7 +1,25 @@
 
+
+/// Sum on a column in a DataFrame
 #[macro_export]
 macro_rules! sum {
     ($df:ident, $column:ident) => (
         $df.data.iter().map(|row| row.$column).sum()
+    )
+}
+
+/// Map a function over a specific column of the dataframe
+#[macro_export]
+macro_rules! map_column {
+    ($df:ident, $column:ident, $func:expr) => (
+        $df.data.iter().map(|r| $func(r.$column))
+    )
+}
+
+/// Map a function over the rows of the dataframe
+#[macro_export]
+macro_rules! map {
+    ($df:ident, $func:expr) => (
+        $df.data.iter().map($func)
     )
 }
--- tests/inserts.rs
+++ tests/inserts.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 
-use fuselage::{DataFrame, sum};
+use fuselage::{DataFrame, sum, map_column, map};
 
 
 #[derive(Deserialize)]
@@ -18,10 +18,20 @@ impl Record {
 
 
 #[test]
-fn basic() {
+fn basic_macros() {
     let mut df = DataFrame::<Record>::new();
     df.push(Record::new());
 
     let v: usize = sum!(df, age);
     assert_eq!(v, 30);
+
+    df.push(Record::new());
+    let v: usize = sum!(df, age);
+    assert_eq!(v, 60);
+
+    let v: Vec<usize> = map_column!(df, age, |n: usize| {n * 2}).collect();
+    assert_eq!(v, vec![60, 60]);
+
+    let v: Vec<usize> = map!(df, |row| {row.age * 3}).collect();
+    assert_eq!(v, vec![90, 90]);
 }
