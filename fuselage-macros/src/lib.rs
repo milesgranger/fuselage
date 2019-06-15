82ce712ad908e552401d7e2d85342ed2951db07b
Miles Granger <miles59923@gmail.com>
2019-06-15 19:28:31 +0200

    Checkpoint - working proto

--- fuselage-macros/src/lib.rs
+++ fuselage-macros/src/lib.rs
@@ -1,11 +1,25 @@
 extern crate proc_macro;
-use syn::{parse_macro_input};
+use syn;
 use quote::quote;
 
 #[proc_macro_attribute]
 pub fn sum(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let args = proc_macro2::TokenStream::from(args);
-    let input = proc_macro2::TokenStream::from(input);
+    let args = syn::parse_macro_input!(args as syn::ExprLit);
+    let input = syn::parse_macro_input!(input as syn::ItemFn);
 
-    input.into()
+    let fn_name = &input.ident;
+
+    let lit = match &args.lit {
+        syn::Lit::Str(s) => s,
+        _ => panic!()
+    };
+
+    let result = quote! {
+        fn #fn_name () {
+            println!("{}", #lit);
+        }
+    };
+
+    result.into()
+
 }
--- fuselage-macros/tests/simple.rs
+++ fuselage-macros/tests/simple.rs
@@ -3,12 +3,11 @@ use fuselage_macros::sum;
 #[test]
 fn df_sum() {
 
-
-    #[sum("hello")]
+    #[sum("hello there")]
     fn my_simple_function() -> usize {
         4
     }
 
-
+    my_simple_function();
 
 }
